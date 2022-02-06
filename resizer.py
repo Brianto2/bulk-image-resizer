6df512776d00e3f6ff6e1274861475dec62abf57
Brianto2 <91679392+Brianto2@users.noreply.github.com>
2022-02-05 20:07:54 -0500

    GUI update
    Added GUI.
    Prevents resizing without a folder specified.
    Warns user if resize width is less than 800px
    Added confirmation before resizing

--- resizer.py
+++ resizer.py
@@ -1,30 +1,115 @@
-# Resizes image to a fixed width while maintaining the aspect ratio
-import os.path
-from tkinter import Tk
-from tkinter.filedialog import askdirectory
-
+import os
+import shutil
+from tkinter import *
+from tkinter import ttk, messagebox
+from tkinter import filedialog
 from PIL import Image
 
-# File path, warning: python will go into each folder inside each folder and modify each file
-# Unless you want to change all the folders too make sure this is the only folder inside the folder
-Tk().withdraw()  # hides ui since we only need the open folder ui
-f = askdirectory()
 
-# grabs all the images in the given folder above
-for file in os.listdir(f):
-    # open the image
-    f_img = f + "/" + file
-    img = Image.open(f_img)
+# Main frame
+def UI():
 
-    basewidth = 1920
+    gui = Tk()
+    gui.geometry("375x175")
+    gui.title("Bulk image resizer")
+    # Create a style
+    style = ttk.Style(gui)
 
-    # Calculate resize percentage
-    wpercent = (basewidth / float(img.size[0]))
+    # Set the theme with the theme_use method
+    style.theme_use('vista')
 
-    # Calculate new high using above
-    hsize = int((float(img.size[1]) * float(wpercent)))
+    src_path = StringVar()
+    width = StringVar()
+    dpi = StringVar()
+    options = [
+        600,
+        300,
+        144,
+        72
+    ]
 
-    # Resize and save image
-    img = img.resize((basewidth, hsize), Image.ANTIALIAS)
-    img.save(f_img, dpi=(300, 300))
-    img.close()
+    width.set(1920)
+    dpi.set("300")
+
+    # first row, source folder
+    s_label = Label(gui, text="Source Folder")
+    s_entry = Entry(gui, textvariable=src_path)
+    s_browse = ttk.Button(gui, text="Browse Folder", command=lambda: getFolderPath(src_path))
+
+    s_label.grid(row=0, column=0, padx=(10, 10), pady=(10, 5))
+    s_entry.grid(row=0, column=1, padx=(10, 10), pady=(10, 5))
+    s_browse.grid(row=0, column=2, padx=(10, 10), pady=(10, 5))
+
+    # second row, resize width
+    w_label = ttk.Label(gui, text="Width (Pixels)")
+    w_entry = ttk.Entry(gui, textvariable=width)
+
+    w_label.grid(row=1, column=0, padx=(10, 10), pady=(10, 5))
+    w_entry.grid(row=1, column=1, padx=(10, 10), pady=(10, 5))
+
+    # third row, dpi
+    d_label = ttk.Label(gui, text="Quality (DPI)")
+    d_option = ttk.OptionMenu(gui, dpi, *options)
+
+    d_label.grid(row=2, column=0, padx=(10, 10), pady=(10, 5))
+    d_option.grid(row=2, column=1, padx=(10, 10), pady=(10, 5))
+
+    # start button
+    c = ttk.Button(gui, text="move images", command=lambda: resize(src_path, width, dpi))
+    c.grid(row=2, column=2, padx=(10, 10), pady=(10, 10))
+
+    return gui
+
+
+# Sets the directory when the button is clicked
+def getFolderPath(folder_path):
+    folder_selected = filedialog.askdirectory()
+    folder_path.set(folder_selected)
+
+
+# bulk resize images
+# vars: dir = directory, width = width to resize to, res = image resolution in dpi (dots per inch)
+
+def resize(folder, width, res):
+    folder = folder.get()
+    width = int(width.get())
+    res = res.get()
+
+    # Don't do anything unless directory path is specified
+    if folder == "":
+        messagebox.showwarning(title="BW Image Mover", message="Input source folders")
+        return
+
+    # Small image warning
+    if width < 800:
+        o = messagebox.askokcancel(title="BW Image Mover", message="Resize width is less than 800px Continue?")
+        if not o:
+            return
+
+    # Get the user to confirm
+    mstring = "Resizing Images in " + folder + " to a width of " + str(width) + " at " + res + \
+              "dpi. THIS CANNOT BE UNDONE."
+    option = messagebox.askokcancel(title="BW Image Mover", message=mstring)  # returns True or False
+    if not option:
+        return
+
+    # grabs all the images in the given folder above
+    for file in os.listdir(folder):
+        # open the image
+        f_img = folder + "/" + file
+        img = Image.open(f_img)
+
+        # Calculate resize percentage
+        wpercent = (width / float(img.size[0]))
+
+        # Calculate new high using above
+        hsize = int((float(img.size[1]) * float(wpercent)))
+
+        # Resize and save image
+        img = img.resize((width, hsize), Image.ANTIALIAS)
+        img.save(f_img, dpi=(res, res))
+        img.close()
+
+
+mainframe = UI()
+mainframe.mainloop()
